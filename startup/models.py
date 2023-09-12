1756f4c691c90548f71b27c441262a8625cc85e6
ogabek <quvvatullayev@gmail.com>
2023-09-12 14:47:39 +0500

    create startup model

--- startup/models.py
+++ startup/models.py
@@ -2,7 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class StartupModel(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    startup_name = models.CharField(max_length=200)
+    startup_price = models.FloatField(default=0.0)
+    startup_about = models.TextField(max_length=100000)
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updeated = models.DateTimeField(auto_now=True, blank=True, null=True)
 
 class UserModel(User):
     user_price = models.FloatField(default=0.0)
