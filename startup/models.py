708835ff0b2354690f494201a737414bb19c64b8
ogabek <quvvatullayev@gmail.com>
2023-09-12 14:39:09 +0500

    create user model

--- startup/models.py
+++ startup/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class StartupModel(models.Model):
+    pass
+
+class UserModel(User):
+    user_price = models.FloatField(default=0.0)
+
+    def __str__(self) -> str:
+        return self.username
